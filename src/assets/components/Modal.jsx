43caaafaa5fd782c746ea4451aea644b4524195e
giorgi merkviladze <giorgimerkviladze@macbooks-MacBook-Pro-2.local>
2023-11-18 16:34:44 +0400

    added costum hook

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -1,14 +1,20 @@
+import { useRef } from "react";
 import { SModal, SmodalWrapper, SModalHeader } from "./modal.styled.js";
 import { AiFillCloseCircle } from "react-icons/ai";
+import { useOnClickOutside } from "../../hooks/useOnClickOutside.js";
 
 export function Modal({ Visible = true, children, title, onClose }) {
+  const modalRef = useRef(null);
+
+  useOnClickOutside(modalRef, () => onClose());
+
   if (!Visible) {
     return <></>;
   }
 
   return (
     <SmodalWrapper>
-      <SModal>
+      <SModal ref={modalRef}>
         <SModalHeader>
           <h3>{title}</h3>
           <button onClick={onClose}>
